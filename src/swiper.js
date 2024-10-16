97e44ee3f8c3cdbcd2d9f7c177fde9b2dcbb7d80
TsvetkovM <149527029+TsvetkovM@users.noreply.github.com>
2024-10-16 11:01:08 +0300

    completed swiper

--- src/swiper.js
+++ src/swiper.js
@@ -17,7 +17,7 @@ var swiper = new Swiper(".mySwiper", {
         },
     },
     coverflowEffect: {
-        rotate: 50,
+        rotate: 40,
         stretch: 0,
         depth: 100,
         modifier: 1,
